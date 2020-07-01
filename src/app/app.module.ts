adb26a2d2af89cfb1afd3127215957d42c5dc93b
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-06-30 22:37:04 -0300

    aula_02 | roteamento, componentes, diretivas e validações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
-//Importação e referência ao HeaderComponent
 import { HeaderComponent } from './components/header/header.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
+import { LoginComponent } from './modules/login/login.component';
+
+import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import { ModuloRoteamento } from './app.routes';
+import { CmailFormGroupComponent } from './components/cmail-form-group/cmail-form-group.component'; //Importação da referência
+import { CmailFormFieldDirective } from './components/cmail-form-group/cmail-form-field.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CaixaDeEntradaComponent,
+    LoginComponent,
+    CadastroComponent,
+    CmailFormGroupComponent,
+    CmailFormFieldDirective // importante utilizar intelisense para importação ser carregada acima!
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ModuloRoteamento, //módulo de roteamento aqui!
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  formCadastro = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    username: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required]),
+    avatar: new FormControl()
+  })
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  handleCadastrarUsuario(){
+    if(this.formCadastro.valid){
+      console.log(this.formCadastro.value);
+      this.formCadastro.reset();
+    }
+    else{
+      this.validarTodosOsCamposDoFormulario(this.formCadastro);
+    }
+  }
+
+  validarTodosOsCamposDoFormulario(form: FormGroup){
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field);
+      control.markAsTouched({ onlySelf: true});
+    })
+
+  }
+}
